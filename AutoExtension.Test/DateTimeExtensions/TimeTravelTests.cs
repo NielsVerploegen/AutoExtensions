5e3b10f23478fad0ed77bb3b87fd1bb9e622b06c
Niels <Niels@DESKTOP-0PTPA12>
2016-10-02 13:54:01 +0200

    Made some timetravel extension methods for the datetime class.

--- AutoExtension.Test/DateTimeExtensions/TimeTravelTests.cs
+++ AutoExtension.Test/DateTimeExtensions/TimeTravelTests.cs
@@ -8,9 +8,39 @@ namespace AutoExtension.Test.DateTimeExtensions
     public class TimeTravelTests
     {
         [Test]
-        public void NextMonthReturnsTheCorrectMonthNumber()
+        public void NextYearReturnsTheCorrectYear()
         {
-           Assert.AreEqual(2, new DateTime(2015, 1, 1).NextMonth());
-        } 
+            Assert.AreEqual(new DateTime(2010,1,1), new DateTime(2009,1,1).NextYear());
+        }
+
+        [Test]
+        public void PreviousYearReturnsTheCorrectYear()
+        {
+            Assert.AreEqual(new DateTime(2010,1,1), new DateTime(2011,1,1).PreviousYear());
+        }
+
+        [Test]
+        public void NextMonthReturnsTheCorrectMonth()
+        {
+            Assert.AreEqual(new DateTime(2015, 2, 1), new DateTime(2015, 1, 1).NextMonth());
+        }
+
+        [Test]
+        public void PreviousMonthReturnsTheCorrectMonth()
+        {
+            Assert.AreEqual(new DateTime(2014, 12, 1), new DateTime(2015, 1, 1).PreviousMonth());
+        }
+
+        [Test]
+        public void TomorrowReturnsTheCorrectDay()
+        {
+            Assert.AreEqual(new DateTime(2010,1,1), new DateTime(2009,12,31).Tomorrow() );
+        }
+
+        [Test]
+        public void YesterdayReturnsTheCorrectDay()
+        {
+            Assert.AreEqual(new DateTime(2009,12,31), new DateTime(2010,1,1).YesterDay());    
+        }
     }
 }
--- AutoExtension/DateTimeExtensions/TimeTravel.cs
+++ AutoExtension/DateTimeExtensions/TimeTravel.cs
@@ -2,18 +2,71 @@
 
 namespace AutoExtension.DateTimeExtensions
 {
+    /// <summary>
+    /// TimeTravel Extensions for the DateTime class.
+    /// </summary>
     public static class TimeTravel
     {
-
-        public static int NextMonth(this DateTime value)
+        #region public methods
+        /// <summary>
+        /// Increase the DateTime value with one year.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Next Year.</returns>
+        public static DateTime NextYear(this DateTime value)
         {
-            return value.AddMonths(1).Month;
+            return value.AddYears(1);
         }
 
-        public static int PreviousMonth(this DateTime value)
+        /// <summary>
+        /// Decrease the DateTime value with one year.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Previous Year.</returns>
+        public static DateTime PreviousYear(this DateTime value)
         {
-            return value.AddMonths(-1).Month;
+            return value.AddYears(-1);
         }
 
+        /// <summary>
+        /// Increase the DateTime value with one month.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Next Month.</returns>
+        public static DateTime NextMonth(this DateTime value)
+        {
+            return value.AddMonths(1);
+        }
+
+        /// <summary>
+        /// Decrease the DateTime value with one month.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Previous Month.</returns>
+        public static DateTime PreviousMonth(this DateTime value)
+        {
+            return value.AddMonths(-1);
+        }
+
+        /// <summary>
+        /// Increase the DateTime value with one day.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Tomorrow.</returns>
+        public static DateTime Tomorrow(this DateTime value)
+        {
+            return value.AddDays(1);
+        }
+
+        /// <summary>
+        /// Decrease the DateTime value with one day.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>Yesterday.</returns>
+        public static DateTime YesterDay(this DateTime value)
+        {
+            return value.AddDays(-1);
+        }
+        #endregion
     }
 }
