6e009355afef7621506d9245efd8f346a2ed20b5
Niels <Niels@DESKTOP-0PTPA12>
2016-10-02 13:06:59 +0200

    First datetime extension

---
+++ AutoExtension.Test/DateTimeExtensions/TimeTravelTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoExtension.DateTimeExtensions;
+using NUnit.Framework;
+using System;
+
+namespace AutoExtension.Test.DateTimeExtensions
+{
+    [TestFixture]
+    public class TimeTravelTests
+    {
+        [Test]
+        public void NextMonthReturnsTheCorrectMonthNumber()
+        {
+           Assert.AreEqual(2, new DateTime(2015, 1, 1).NextMonth());
+        } 
+    }
+}
---
+++ AutoExtension/DateTimeExtensions/TimeTravel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AutoExtension.DateTimeExtensions
+{
+    public static class TimeTravel
+    {
+
+        public static int NextMonth(this DateTime value)
+        {
+            return value.AddMonths(1).Month;
+        }
+
+        public static int PreviousMonth(this DateTime value)
+        {
+            return value.AddMonths(-1).Month;
+        }
+
+    }
+}
